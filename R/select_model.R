9d2b693e0a9f6c78a00472a2a6866b1c430ce024
placeboo <jiaqiyin1014@gmail.com>
2019-09-06 01:28:54 -0700

    change comment

--- R/select_model.R
+++ R/select_model.R
@@ -1,12 +1,12 @@
 #' Select the optimal model
 #'
-#' @description double seasonal exponential smoothing method is implemented. If grid.search = TRUE, gril search is applied by searching parameters around the "first attemp" parameters. \code{search.length} and \code{length.out} are for grill search. The search is based on the minimum mean absolute percentage error with parallel computing. More details about parallel computing can be found in \code{doParallel}.
+#' @description double seasonal exponential smoothing method is implemented. If grid.search = TRUE, grid search is applied by searching parameters around the "first attemp" parameters. \code{search.length} and \code{length.out} are for grid search. The search is based on the minimum mean absolute percentage error with parallel computing. More details about parallel computing can be found in \code{doParallel}.
 #'
 #' @param train.y A numeric vector for training.
 #' @param valid.y A numeric vector for testing. If \code{grid.search = NULL}, \code{valid.y = NULL}.
 #' @param s1 Period of the shorter seasonal period.
 #' @param s2 Period of the longer seasonal period.
-#' @param grid.search If TRUE, a grill search is applied.
+#' @param grid.search If TRUE, a grid search is applied.
 #' @param search.length Gril search parameter. Only used if \code{grid.search = TRUE}. It is the proportion decreasing/increasing of the first attemp parameters. For example \code{search.length = c(-0.5, 0.5)}, if the first attemp parameter is 0.1, then the searching window is from \code{0.1 * (1 - 0.5)} to \code{0.1 * (1+ 0.5)}
 #' @param length.out Gril search parameter. Only used if \code{grid.search = TRUE}. It is the desired length of search sequence.
 #'
@@ -40,7 +40,7 @@ select_model = function(train.y,
 
 
     org.m = dshw(train.y, period1 = s1, period2 = s2)
-    # don't want to do gril search
+    # don't want to do grid search
     if (grid.search) {
         no_cores = detectCores()
         cl = makeCluster(no_cores)
@@ -54,7 +54,7 @@ select_model = function(train.y,
         phi0 = org.m$model$phi
         org.vec  = c(alpha0, beta0, gamma0, omega0, phi0, measure_dist(forecast(org.m, h=length(valid.y))$mean, valid.y))
 
-        # build gril
+        # build grid
         alpha.vec = seq(alpha0 * (1 + search.length[1]), min(alpha0 * (1 + search.length[2]), 1), length.out = length.out)
 
         beta.vec = seq(beta0 * (1 + search.length[1]), min(beta0 * (1 + search.length[2]), 1), length.out = length.out)
